0be852056a121b8d90860cb8792f1ddedaa97b0f
Nikolay Minins <nikolay.minins@student.nhlstenden.com>
2025-04-07 16:50:50 +0200

    Slight update on DynamicArray.java

--- structures/DynamicArray.java
+++ structures/DynamicArray.java
@@ -1,61 +1,49 @@
 package structures;
 
 import models.Animal;
-
 import java.util.Arrays;
 
-public class DynamicArray
-{
+public class DynamicArray {
     private Animal[] array;
     private int size;
 
-    public DynamicArray()
-    {
+    public DynamicArray() {
         array = new Animal[10];
         size = 0;
     }
 
-    public void add(Animal animal)
-    {
+    public void add(Animal animal) {
         if (size == array.length) {
-
             array = Arrays.copyOf(array, array.length * 2);
         }
         array[size++] = animal;
     }
 
-    public boolean remove(int id)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            if (array[i].getId() == id)
-            {
+    public boolean remove(int id) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].getId() == id) {
                 System.arraycopy(array, i + 1, array, i, size - i - 1);
                 size--;
+                array[size] = null; // Allow garbage collection
                 return true;
             }
         }
         return false;
     }
 
-    public Animal get(int index)
-    {
-        if (index < 0 || index >= size)
-        {
+    public Animal get(int index) {
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index out of bounds");
         }
         return array[index];
     }
 
-    public int getSize()
-    {
+    public int getSize() {
         return size;
     }
 
-    public void display()
-    {
-        for (int i = 0; i < size; i++)
-        {
+    public void display() {
+        for (int i = 0; i < size; i++) {
             System.out.println(array[i]);
         }
     }
