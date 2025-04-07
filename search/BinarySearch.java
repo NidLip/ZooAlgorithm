047accdc94a12df2f6b4c2580050b15c4c7533bf
Nikolay Minins <nikolay.minins@student.nhlstenden.com>
2025-04-07 17:27:03 +0200

    Added two search methods, not tested

--- search/BinarySearch.java
+++ search/BinarySearch.java
@@ -1,5 +1,21 @@
 package search;
 
-public class BinarySearch
-{
+import java.util.Comparator;
+
+public class BinarySearch {
+    public static <T> int search(T[] array, T target, Comparator<T> comparator) {
+        int left = 0, right = array.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int cmp = comparator.compare(array[mid], target);
+            if (cmp == 0) {
+                return mid;
+            } else if (cmp < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
--- search/SequentialSearch.java
+++ search/SequentialSearch.java
@@ -1,5 +1,14 @@
 package search;
 
-public class SequentialSearch
-{
+import java.util.Comparator;
+
+public class SequentialSearch {
+    public static <T> int search(T[] array, T target, Comparator<T> comparator) {
+        for (int i = 0; i < array.length; i++) {
+            if (comparator.compare(array[i], target) == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
